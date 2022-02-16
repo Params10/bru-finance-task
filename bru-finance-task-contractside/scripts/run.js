eae981ea4b5e588a586519d8748a0ec49b90f1a3
Params10 <48449191+Params10@users.noreply.github.com>
2022-02-16 23:01:51 +0530

    Update run.js

--- bru-finance-task-contractside/scripts/run.js
+++ bru-finance-task-contractside/scripts/run.js
@@ -3,28 +3,23 @@ const { utils } = require("ethers");
 async function main() {
     const baseTokenURI = "ipfs://QmZbWNKJPAjxXuNFSEaksCJVd1M6DaKQViJBYPK2BdpDEP/";
 
-    // Get owner/deployer's wallet address
     const [owner] = await hre.ethers.getSigners();
 
-    // Get contract that we want to deploy
     const contractFactory = await hre.ethers.getContractFactory("NFTS");
 
-    // Deploy contract with the correct constructor arguments
+
     const contract = await contractFactory.deploy(baseTokenURI);
 
-    // Wait for this transaction to be mined
+
     await contract.deployed();
 
-    // Get contract address
+
     console.log("Contract deployed to:", contract.address);
 
-    // Reserve NFTs
 
-    // Mint 3 NFTs by sending 0.03 ether
     txn = await contract.mintNFTs(10, { value: utils.parseEther('0.03') });
     await txn.wait()
 
-    // Get all token IDs of the owner
     let tokens = await contract.tokensOfOwner(owner.address)
     console.log("Owner has tokens: ", tokens);
 
